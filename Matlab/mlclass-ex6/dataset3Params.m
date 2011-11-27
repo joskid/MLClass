9db628ef12940dba92ca2c7a44c8ebfaebba9907
Gustavo Guerra <gustavo@codebeside.org>
2011-11-27 22:09:30 +0000

    Matlab version of exercise 6 - parameters

--- Matlab/mlclass-ex6/dataset3Params.m
+++ Matlab/mlclass-ex6/dataset3Params.m
@@ -23,11 +23,23 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+parametersToTry = [0.01;0.03;0.1;0.3;1;3;10;30];
+error = 999999999;
 
-
-
-
-
+for i = 1:size(parametersToTry,1)
+    currentC = parametersToTry(i);
+    for j = 1:size(parametersToTry,1)
+        currentSigma = parametersToTry(j);
+        model = svmTrain(X, y, currentC, @(x1, x2) gaussianKernel(x1, x2, currentSigma));
+        predictions = svmPredict(model, Xval);
+        currentError = mean(double(predictions ~= yval));
+        if currentError < error
+            error = currentError;
+            C = currentC;
+            sigma = currentSigma;
+        end
+    end
+end
 
 % =========================================================================
 
